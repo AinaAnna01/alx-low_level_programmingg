a9d517f7e55e12e1cf72faffd64c01418488ac6d
AinaAnna01 <ainaanna702@gmail.com>
2022-09-08 15:33:38 -0700

    printing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Print a text
+ * main - Prints a text
  *
  * Return: 0
  */
-
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0)
